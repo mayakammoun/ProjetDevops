6e5680dfcedf3618728c4a116f711b992954004d
nourcheneriahi <nourchene.riahi@esprit.tn>
2022-11-12 18:34:13 +0100

    Update IStockService.java

--- tpAchatProject/src/main/java/com/esprit/examen/services/IStockService.java
+++ tpAchatProject/src/main/java/com/esprit/examen/services/IStockService.java
@@ -2,16 +2,17 @@ package com.esprit.examen.services;
 
 import java.util.List;
 import com.esprit.examen.entities.Stock;
+import com.esprit.examen.entities.dto.StockDTO;
 
 public interface IStockService {
 
 	List<Stock> retrieveAllStocks();
 
-	Stock addStock(Stock s);
+	Stock addStock(StockDTO s);
 
 	void deleteStock(Long id);
 
-	Stock updateStock(Stock u);
+	Stock updateStock(StockDTO u);
 
 	Stock retrieveStock(Long id);
 
