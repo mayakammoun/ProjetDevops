fc7c113203373d9e6fd17f4515afdfaa83b993e3
nourcheneriahi <nourchene.riahi@esprit.tn>
2022-11-12 18:37:42 +0100

    Update SecteurActivite.java

--- tpAchatProject/src/main/java/com/esprit/examen/entities/SecteurActivite.java
+++ tpAchatProject/src/main/java/com/esprit/examen/entities/SecteurActivite.java
@@ -23,7 +23,6 @@ public class SecteurActivite implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idSecteurActivite;
