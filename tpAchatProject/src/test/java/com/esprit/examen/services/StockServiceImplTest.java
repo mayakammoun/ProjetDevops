5b220be53076ff194dd5b9bdbfefbea95346c684
nourcheneriahi <nourchene.riahi@esprit.tn>
2022-11-12 18:33:46 +0100

    Update Stockimpl

--- tpAchatProject/src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ tpAchatProject/src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,52 +1,114 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
-import static org.junit.Assert.*;
-import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
+import com.esprit.examen.entities.dto.StockDTO;
+import com.esprit.examen.repositories.StockRepository;
+import com.esprit.examen.services.StockServiceImpl;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class StockServiceImplTest {
-	@Autowired
-	IStockService stockService;
-	
-	@Test
-	public void testAddStock() {
-	//	List<Stock> stocks = stockService.retrieveAllStocks();
-	//	int expected=stocks.size();
-		Stock s = new Stock("stock test",10,100);
-		Stock savedStock= stockService.addStock(s);
-		
-	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
-		assertNotNull(savedStock.getLibelleStock());
-		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
-	@Test
-	public void testAddStockOptimized() {
+	@Mock
+	private StockRepository stockRepository;
+	@InjectMocks
+	private IStockService stockService = new StockServiceImpl();
+	private Stock s1;
+	private Stock s2;
+	ModelMapper modelMapper;
+	@BeforeEach
+	public void init() {
+		this.s1 = new Stock();
+		this.s1.setIdStock(0L);
+		this.s1.setLibelleStock("Test 1");
+		this.s1.setQte(11);
+		this.s1.setQteMin(11);
 
-		Stock s = new Stock("stock test",10,100);
-		Stock savedStock= stockService.addStock(s);
-		assertNotNull(savedStock.getIdStock());
-		assertSame(10, savedStock.getQte());
-		assertTrue(savedStock.getQteMin()>0);
-		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
-	
+		this.s2 = new Stock();
+		this.s2.setIdStock(1L);
+		this.s2.setLibelleStock("Test 2");
+		this.s2.setQte(22);
+		this.s2.setQteMin(22);
+
+		this.modelMapper = new ModelMapper();
+	}
 	@Test
-	public void testDeleteStock() {
-		Stock s = new Stock("stock test",30,60);
-		Stock savedStock= stockService.addStock(s);
-		stockService.deleteStock(savedStock.getIdStock());
-		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
+	@DisplayName("Test Add to Stock")
+	public void testAddStock() {
+		init();
+		when(stockRepository.save(any(Stock.class))).thenReturn(s1);
+		StockDTO sdto = modelMapper.map(s1, StockDTO.class);
+		Stock snew = stockService.addStock(sdto);
+		assertNotNull(snew);
+		assertThat(snew.getLibelleStock()).isEqualTo("Test 1");
 	}
 
+	@Test
+	@DisplayName("Test Retrieve from stock")
+	public void testRetrieveStock() {
+		init();
+		List<Stock> list = new ArrayList<>();
+		list.add(s1);
+		list.add(s2);
+		when(stockRepository.findAll()).thenReturn(list);
+		List<Stock> Stocks = stockService.retrieveAllStocks();
+		assertEquals(2, Stocks.size());
+		assertNotNull(Stocks);
+	}
+	@Test
+	@DisplayName("Test Get Stock by id")
+	public void testGetStockById() {
+		init();
+		when(stockRepository.save(any(Stock.class))).thenReturn(s1);
+		StockDTO srm=modelMapper.map(s1, StockDTO.class);
+		Stock snew=stockService.addStock(srm);
+		when(stockRepository.findById(anyLong())).thenReturn(Optional.of(s1));
+		Stock existingStock = stockService.retrieveStock(snew.getIdStock());
+		assertNotNull(existingStock);
+		assertThat(existingStock.getIdStock()).isNotNull();
+	}
+	@Test
+	@DisplayName("Test Delete from stock")
+	public void testDeleteStock() {
+		init();
+		Long StockId =1L;
+		when(stockRepository.findById(anyLong())).thenReturn(Optional.of(s1));
+		doNothing().when(stockRepository).deleteById(anyLong());
+		stockService.deleteStock(StockId);
+		verify(stockRepository, times(1)).deleteById(anyLong());
+	}
+
+	@Test
+	@DisplayName("Test Update Stock")
+	public void testUpdateStock() {
+		init();
+		when(stockRepository.findById(anyLong())).thenReturn(Optional.of(s1));
+		when(stockRepository.save(any(Stock.class))).thenReturn(s1);
+		s1.setLibelleStock("test");
+		StockDTO sdto=modelMapper.map(s1, StockDTO.class);
+		Stock exisitingStock = stockService.updateStock(sdto);
+		assertNotNull(exisitingStock);
+		assertEquals("test", exisitingStock.getLibelleStock());
+
+	}
 }
-*/
