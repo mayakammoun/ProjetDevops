112c051f9b13a4419d99126d8edb48837d1fc634
nourcheneriahi <nourchene.riahi@esprit.tn>
2022-11-12 18:34:06 +0100

    Update StockServiceImpl.java

--- tpAchatProject/src/main/java/com/esprit/examen/services/StockServiceImpl.java
+++ tpAchatProject/src/main/java/com/esprit/examen/services/StockServiceImpl.java
@@ -3,6 +3,8 @@ package com.esprit.examen.services;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import com.esprit.examen.entities.dto.StockDTO;
+import com.esprit.examen.services.IStockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.esprit.examen.entities.Stock;
@@ -21,7 +23,7 @@ public class StockServiceImpl implements IStockService {
 	public List<Stock> retrieveAllStocks() {
 		// récuperer la date à l'instant t1
 		log.info("In method retrieveAllStocks");
-		List<Stock> stocks = (List<Stock>) stockRepository.findAll();
+		List<Stock> stocks = stockRepository.findAll();
 		for (Stock stock : stocks) {
 			log.info(" Stock : " + stock);
 		}
@@ -32,10 +34,16 @@ public class StockServiceImpl implements IStockService {
 	}
 
 	@Override
-	public Stock addStock(Stock s) {
+	public Stock addStock(StockDTO s) {
 		// récuperer la date à l'instant t1
 		log.info("In method addStock");
-		return stockRepository.save(s);
+		return stockRepository.save(Stock.builder()
+						.idStock(s.getIdStock())
+				.libelleStock(s.getLibelleStock())
+				.qte(s.getQte())
+				.qteMin(s.getQteMin())
+				.produits(s.getProduits())
+				.build());
 		
 	}
 
@@ -47,9 +55,14 @@ public class StockServiceImpl implements IStockService {
 	}
 
 	@Override
-	public Stock updateStock(Stock s) {
+	public Stock updateStock(StockDTO s) {
 		log.info("In method updateStock");
-		return stockRepository.save(s);
+		return stockRepository.save(Stock.builder()
+				.libelleStock(s.getLibelleStock())
+				.qte(s.getQte())
+				.qteMin(s.getQteMin())
+				.produits(s.getProduits())
+				.build());
 	}
 
 	@Override
@@ -69,18 +82,16 @@ public class StockServiceImpl implements IStockService {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		Date now = new Date();
 		String msgDate = sdf.format(now);
-		String finalMessage = "";
 		String newLine = System.getProperty("line.separator");
-		List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
+		List<Stock> stocksEnRouge =  stockRepository.retrieveStatusStock();
+		StringBuilder finalMessage = new StringBuilder();
 		for (int i = 0; i < stocksEnRouge.size(); i++) {
-			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
-					+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
-					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
-					+ newLine;
-
+			finalMessage.append(newLine).append(finalMessage).append(msgDate).append(newLine).append(": le stock ").
+			append(stocksEnRouge.get(i).getLibelleStock()).append(" a une quantité de ").append(stocksEnRouge.get(i).getQte())
+					.append(" inférieur à la quantité minimale a ne pas dépasser de ").append(stocksEnRouge.get(i).getQteMin())
+					.append(newLine);
 		}
-		log.info(finalMessage);
-		return finalMessage;
+		return finalMessage.toString();
 	}
 
 }
